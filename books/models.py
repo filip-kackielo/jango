e22621782155f691fcb3b8a136b4565148ef56e4
filip.kackielo <filip.kackielo-wargulewski@infotech.edu.pl>
2023-09-19 12:12:21 +0200

    lekcja2

--- books/models.py
+++ books/models.py
@@ -9,7 +9,14 @@ class Author(models.Model):
     year_of_birth = models.IntegerField(null=True)
 
 
+    # dodaj poniższą metodę
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.category_name}'
 
 
